6854cb13d81ae2d04ced4bb387ba5106abd027b5
kimutaifaith <128345240+kimutaifaith@users.noreply.github.com>
2025-05-24 18:24:34 +0300

    Create 1-execute.py

---
+++ python-context-async-perations-0x02/1-execute.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+class ExecuteQuery:
+    def __init__(self, db_name, query, params=None):
+        self.db_name = db_name
+        self.query = query
+        self.params = params or ()
+        self.conn = None
+        self.cursor = None
+        self.result = None
+
+    def __enter__(self):
+        self.conn = sqlite3.connect(self.db_name)
+        self.cursor = self.conn.cursor()
+        self.cursor.execute(self.query, self.params)
+        self.result = self.cursor.fetchall()
+        return self.result
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.conn:
+            if exc_type is not None:
+                self.conn.rollback()
+            else:
+                self.conn.commit()
+            self.cursor.close()
+            self.conn.close()
