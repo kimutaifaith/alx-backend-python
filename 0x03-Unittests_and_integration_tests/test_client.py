410971bf83a02e272d7947cbf208d3fc69926649
kimutaifaith <128345240+kimutaifaith@users.noreply.github.com>
2025-05-27 14:44:03 +0300

    Update test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -79,6 +79,17 @@ class TestGithubOrgClient(unittest.TestCase):
             mock_get_json.assert_called_once_with(
                 "https://api.github.com/orgs/test_org/repos"
             )
-
+ @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        """
+        Test GithubOrgClient.has_license returns correct boolean based on
+        repo license key matching the given license_key.
+        """
+        result = GithubOrgClient.has_license(repo, license_key)
+        self.assertEqual(result, expected)
+        
 if __name__ == "__main__":
     unittest.main()
