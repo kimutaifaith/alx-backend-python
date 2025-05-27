38875123c95ab6c06199e5a2826ed79c49442351
kimutaifaith <128345240+kimutaifaith@users.noreply.github.com>
2025-05-27 16:38:48 +0300

    Update urls.py

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -7,6 +7,9 @@ from .views import ConversationViewSet, MessageViewSet
 router = routers.DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversation')
 router.register(r'messages', MessageViewSet, basename='message')
+# Nested router for messages under conversations
+conversations_router = routers.NestedDefaultRouter(router, r'conversations', lookup='conversation')
+conversations_router.register(r'messages', MessageViewSet, basename='conversation-messages')
 
 urlpatterns = [
     path('', include(router.urls)),
