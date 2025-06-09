e1862b0035fe3663b4180c026d2847f967afdbf4
kimutaifaith <chepkemoifaith821@gmail.com>
2025-06-09 11:50:53 +0300

    Create Models.py

---
+++ Django-signals_orm-0x04/Django-Chat/Models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Message(models.Model):
+    sender = models.ForeignKey(User, related_name='sent_messages', on_delete=models.CASCADE)
+    receiver = models.ForeignKey(User, related_name='received_messages', on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    edited = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.sender} → {self.receiver}: {self.content[:30]}"
+class MessageHistory(models.Model):
+    message = models.ForeignKey(Message, related_name='history', on_delete=models.CASCADE)
+    old_content = models.TextField()
+    edited_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"History for Msg ID {self.message.id} at {self.edited_at}"
